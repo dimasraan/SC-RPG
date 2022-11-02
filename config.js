2168fd9ad4d7405d1f5da07ea66e37bfdc44c9e9
dimasraan <47117467+dimasraan@users.noreply.github.com>
2022-11-03 00:03:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -44,15 +44,15 @@ global.ppulsa2 = '-'
 global.psaweria = 'https://saweria.co/raraharsita2'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '17326319049'
-global.nomorown = '6288279268363'
+global.nomorbot = '6287787342055'
+global.nomorown = '6281269024490'
 global.namebot = 'Yuki Adistyラ'
 global.nameown = 'ᴹᴿ᭄ King Of Bear ×፝֟͜×'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6288279268363', 'ᴹᴿ᭄ King Of Bear ×፝֟͜×', 'true']
+  ['6281269024490', 'ᴹᴿ᭄ King Of Bear ×፝֟͜×', 'true']
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
